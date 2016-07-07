08202a9402441f3ea22db5da24ccaee9136dbf3e
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-07 12:04:37 +0300

    Add xml comments.

--- Task1/Task1/JaggedArrayOperation.cs
+++ Task1/Task1/JaggedArrayOperation.cs
@@ -13,7 +13,7 @@ namespace Task1
         /// </summary>
         /// <param name="someJagged">Not sorted array</param>
         /// <returns>Sorted array</returns>
-        public static int[][] JaggedSortRowsBySum(int[][] someJagged)
+        public static int[][] JaggedSortRowsBySumAscend(int[][] someJagged)
         {
            
             for (int i = 0; i < someJagged.Length; i++)
@@ -30,13 +30,34 @@ namespace Task1
             }
             return someJagged;
         }
+        /// <summary>
+        /// Sorting rows of the matrix in descending order of sums elements rows of the matrix`
+        /// </summary>
+        /// <param name="someJagged">Not sorted array</param>
+        /// <returns>Sorted array</returns>
+        public static int[][] JaggedSortRowsBySumDescend(int[][] someJagged)
+        {
 
+            for (int i = 0; i < someJagged.Length; i++)
+            {
+                for (int j = 0; j < someJagged.Length - 1 - i; j++)
+                {
+                    if (someJagged[j].Sum() < someJagged[j + 1].Sum())
+                    {
+                        var temp = someJagged[j];
+                        someJagged[j] = someJagged[j + 1];
+                        someJagged[j + 1] = temp;
+                    }
+                }
+            }
+            return someJagged;
+        }
         /// <summary>
         /// Sorting rows of the matrix in ascending order of max elements rows of the matrix
         /// </summary>
         /// <param name="someJagged"></param>
         /// <returns>Sorted array</returns>
-        public static int[][] JaggedSortRowsByMax(int[][] someJagged)
+        public static int[][] JaggedSortRowsByMaxAscend(int[][] someJagged)
         {
 
             for (int i = 0; i < someJagged.Length; i++)
@@ -54,11 +75,55 @@ namespace Task1
             return someJagged;
         }
         /// <summary>
-        /// 
+        /// Sorting rows of the matrix in descending order of max elements rows of the matrix
         /// </summary>
         /// <param name="someJagged"></param>
         /// <returns>Sorted array</returns>
-        public static int[][] JaggedSortRowsByMin(int[][] someJagged)
+        public static int[][] JaggedSortRowsByMaxDescend(int[][] someJagged)
+        {
+
+            for (int i = 0; i < someJagged.Length; i++)
+            {
+                for (int j = 0; j < someJagged.Length - 1 - i; j++)
+                {
+                    if (someJagged[j].Max() < someJagged[j + 1].Max())
+                    {
+                        var temp = someJagged[j];
+                        someJagged[j] = someJagged[j + 1];
+                        someJagged[j + 1] = temp;
+                    }
+                }
+            }
+            return someJagged;
+        }
+        /// <summary>
+        /// Sorting rows of the matrix in ascending order of min elements rows of the matrix
+        /// </summary>
+        /// <param name="someJagged"></param>
+        /// <returns>Sorted array</returns>
+        public static int[][] JaggedSortRowsByMinAscend(int[][] someJagged)
+        {
+
+            for (int i = 0; i < someJagged.Length; i++)
+            {
+                for (int j = 0; j < someJagged.Length - 1 - i; j++)
+                {
+                    if (someJagged[j].Min() > someJagged[j + 1].Min())
+                    {
+                        var temp = someJagged[j];
+                        someJagged[j] = someJagged[j + 1];
+                        someJagged[j + 1] = temp;
+                    }
+                }
+            }
+            return someJagged;
+        }
+        /// <summary>
+        /// Sorting rows of the matrix in descending order of min elements rows of the matrix
+        /// </summary>
+        /// <param name="someJagged"></param>
+        /// <returns>Sorted array</returns>
+        public static int[][] JaggedSortRowsByMinDescend(int[][] someJagged)
         {
 
             for (int i = 0; i < someJagged.Length; i++)
@@ -77,3 +142,4 @@ namespace Task1
         }
     }
 }
+
