f0a0901d080ef3720ae067f10405dcafc0434b45
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-12 22:00:51 +0300

    Add tests.

--- EPAM.Summer.Day4.Zheldak/Task1.NUnitTests/SortOperationTests.cs
+++ EPAM.Summer.Day4.Zheldak/Task1.NUnitTests/SortOperationTests.cs
@@ -15,42 +15,47 @@ namespace Task1.NUnitTests
         public void SortBySummRowsAscend()
         {
             ISortJagged sortJagged = new SortBySumAscend();
+
             int[][] jaggedArray =
                 {
                     new[] {1,3,5,7,9},
                     new[] {0,2,4,0},
                     new[] {11,22}
                };
-            SortOperation.SortMethod(jaggedArray, sortJagged);
+
             int[][] result =
-                {
+               {
                     new[] {0,2,4,0},
                     new[] {1,3,5,7,9},
                     new[] {11,22}
                };
 
+            SortOperation.SortMethod(jaggedArray, sortJagged);
+           
             CollectionAssert.AreEqual(jaggedArray, result);
         }
-
-
+        
         [Test]
         public void SortByMaxModuleElements()
         {
             ISortJagged sortJagged = new SortByMaxModule();
+
             int[][] jaggedArray =
                 {
                     new[] {1,3,5,7,-9},
                     new[] {11,-22},
                     new[] {0,2,4,0}
                 };
-            SortOperation.SortMethod(jaggedArray, sortJagged);
+
             int[][] result =
-                {
+               {
                    new[] {0,2,4,0},
                    new[] {1,3,5,7,-9},
                    new[] {11,-22},
                };
 
+            SortOperation.SortMethod(jaggedArray, sortJagged);
+           
             CollectionAssert.AreEqual(jaggedArray, result);
         }
 
